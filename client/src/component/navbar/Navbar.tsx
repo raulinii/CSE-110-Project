e61f25cb96253a54bc0632c568f8cc22535cd53e
KaronLan <100540201+KaronLan@users.noreply.github.com>
2024-11-03 12:44:20 -0800

    change botton text into icons

--- client/src/component/navbar/Navbar.tsx
+++ client/src/component/navbar/Navbar.tsx
@@ -1,27 +1,29 @@
 
 import React, { useState } from "react";
-import { useLocation, Link } from "react-router-dom";
+import { FaHome, FaClock, FaUser} from "react-icons/fa";
 import "./Navbar.css";
 
 // Define the type for navigation items
 interface NavItem {
   label: string;
   url: string;
+  icon : any;
 }
 
 const Navbar = () =>{
 
     const navItems: NavItem[] = [
-        { label: "Home", url: "/home" },
-        { label: "Clock", url: "/Clock" },
-        { label: "User", url: "/User" }
+        { label: "Home", url: "/home", icon: <FaHome/> },
+        { label: "Clock", url: "/Clock", icon: <FaClock /> },
+        { label: "User", url: "/User", icon: <FaUser /> }
     ];
     
     // State to track the active item
     const initialUrl =  localStorage.getItem("activeUrl") || '/home';
     const [activeUrl, setActiveUrl] = useState<string>(initialUrl);
     
-    const handleNavClick = (event: React.MouseEvent<HTMLButtonElement>,url: string) => {
+    // Set active URL
+    const handleNavClick = (url: string) => {
         setActiveUrl(url);
         localStorage.setItem("activeUrl", url);
         window.location.href = url;
@@ -29,14 +31,15 @@ const Navbar = () =>{
 
     return (
     <nav className="navbar">
+
         <ul className="nav-list">
         {navItems.map((item) => (
             <li key={item.url} className="nav-item">
             <button
                 className={`nav-button ${activeUrl === item.url ? "active" : ""}`}
-                onClick={(event) => handleNavClick(event,item.url)}
+                onClick={() => handleNavClick(item.url)}
             >
-                {item.label}
+                <span className="icon">{item.icon}</span>
             </button>
             </li>
         ))}
