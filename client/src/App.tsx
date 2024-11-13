f64ba5857202f5fcfc3c0f9b223ef6e473cf4144
RunnnnWang <130102197+RunnnnWang@users.noreply.github.com>
2024-11-12 19:50:45 -0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+
+
 import React from 'react';
 import MainPage from "./components/main/mainPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PlayerPage from "./components/player/PlayerPage";
+
 
 const App = () => {
-  // HINT: Wrap the MyBudgetTracker component with AppContextProvider
   return (
-    <MainPage/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/player/:category" element={<PlayerPage />} />
+      </Routes>
+    </Router>
   );
-  
 };
 
 export default App;
