f3bc57521044233d697ac324624e12fd8f0729d2
rudy osuna <ruuudyosuna@gmail.com>
2024-11-22 10:55:13 -0800

    fixed image issue

--- client/src/component/mainpage/mainPage.tsx
+++ client/src/component/mainpage/mainPage.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./mainPage.css";
-import logo from "./logo.png";
-import mapIcon from "./map.png";
-import capsIcon from "./caps.png";
-import ucsdIcon from "./ucsd.webp";
+import logo from "./images/logo.png";
+import mapIcon from "./images/map.png";
+import capsIcon from "./images/caps.png";
+import ucsdIcon from "./images/ucsd.webp";
 import WelcomePopup from "./WelcomePopup"; // Import the popup component
 
 interface Category {
