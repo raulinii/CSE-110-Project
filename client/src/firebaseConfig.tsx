d83251621ed3570d5334fa52b1da1ead65498ce9
Peeyush-Jha <pejha@ucsd.edu>
2024-11-24 17:58:46 -0800

    Added Tests for login and signup

--- client/src/firebaseConfig.tsx
+++ client/src/firebaseConfig.tsx
@@ -8,12 +8,8 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY as string,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN as string,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID as string,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET as string,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID as string,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID as string,
+  apiKey: "AIzaSyA0m3BGx6DKaOUFHWBI_wZ2WDOmvpTer44",
+    projectId: "mindfulu-110",
 };
 
 // Initialize Firebase
