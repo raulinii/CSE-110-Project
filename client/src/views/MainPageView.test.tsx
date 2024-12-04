cab8f90c704363c6bff0274342736151ce1304b8
RunWang0402 <mwwpositive1000@gmail.com>
2024-12-04 15:32:18 -0800

    added test for main page routing to other pages

--- client/src/views/MainPageView.test.tsx
+++ client/src/views/MainPageView.test.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from "react-router-dom"
 import MainPage from './MainPageView';
+import SleepVideoView from "./SleepVideoView";
+import StressVideoView from './StressVideoView';
+import FocusVideoView from './FocusVideoView';
 
 
 test("resources CAPS link working", () => {
@@ -34,3 +37,61 @@ test("Meditation Location link working", () => {
         expect(createLink.closest("a")).toHaveAttribute("target", "_blank"); //make sure it opens the link in a new tab securely
               
           });
+
+test("Buttons for sleep, stress, focus page render successfully", () => {
+    render(
+        <MemoryRouter>
+        <MainPage />
+        </MemoryRouter>)
+    
+    const SleepButton = screen.getByText("Sleep");
+    expect(SleepButton).toBeInTheDocument();
+    
+    const StressButton = screen.getByText("Stress");
+    expect(StressButton).toBeInTheDocument();
+
+    
+    const FocusButton = screen.getByText("Focus");
+    expect(FocusButton).toBeInTheDocument();
+
+                    
+        });
+
+test("Route successfully to the Stress Player Page", () => {
+    render(
+        <MemoryRouter>
+        <MainPage />
+        <StressVideoView />
+        </MemoryRouter>)
+    
+    const StressButton = screen.getByText("Stress");
+    fireEvent.click(StressButton);
+    expect(screen.getByText("Relaxation Meditations")).toBeInTheDocument();
+                    
+        });
+
+test("Route successfully to the Sleep Player Page", () => {
+    render(
+        <MemoryRouter>
+        <MainPage />
+        <SleepVideoView />
+        </MemoryRouter>)
+    
+    const SleepButton = screen.getByText("Sleep");
+    fireEvent.click(SleepButton);
+    expect(screen.getByText("Sleep Meditations")).toBeInTheDocument();
+                    
+        });
+
+test("Route successfully to the Focus Player Page", () => {
+    render(
+        <MemoryRouter>
+        <MainPage />
+        <FocusVideoView />
+        </MemoryRouter>)
+    
+    const SleepButton = screen.getByText("Focus");
+    fireEvent.click(SleepButton);
+    expect(screen.getByText("Focus Meditations")).toBeInTheDocument();
+                    
+        });
